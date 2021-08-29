2eddf1f17999f036db0cee090d53475311d33f6c
Irgio Basrewan <irgiob@gmail.com>
2021-08-30 00:16:02 +0700

    switched react to use scss instead of css

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,7 @@ import * as React from "react"
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 
-import "./index.css"
+import "./index.scss"
 
 import { IoIosArrowDown } from "react-icons/io"
 
