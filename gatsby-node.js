1e2fced071fc01c2c3bc8cd1bbb319a29ef873b4
fjoyceline <fionajoyceline@gmail.com>
2021-08-30 14:04:31 +0700

    New host

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,75 +1,77 @@
-const firebase = require('firebase-admin');
-const crypto = require('crypto');
+const firebase = require("firebase-admin")
+const crypto = require("crypto")
 require("dotenv").config({
   path: `.env`,
 })
 
-const getDigest = id =>
-  crypto
-    .createHash('md5')
-    .update(id)
-    .digest('hex');
+const getDigest = id => crypto.createHash("md5").update(id).digest("hex")
 
-exports.sourceNodes = async ({ actions, createNodeId, createContentDigest }) => {
-    const { createNode } = actions
-  
-    types = [
-        {
-          type: 'User',
-          collection: 'Users',
-          map: doc => ({
-            name: doc.name,
-            email: doc.email,
-            lastOpenedOrganization___NODE: doc.lastOpenedOrganization.id,
-            notificationMode: doc.notificationMode,
-            profilePicture: doc.profilePicture,
-            phoneNumber: [doc.phoneNumber.number, doc.phoneNumber.type],
-            organizations___NODE: doc.organizations.map(org => org.id),
-          }),
-        },
-        {
-          type: 'Organization',
-          collection: 'Organizations',
-          map: doc => ({
-            name: doc.name,
-            description: doc.description,
-            profilePicture: doc.profilePicture,
-            admin___NODE: doc.admin.id,
-            members___NODE: doc.members.map(user => user.id),
-          }),
-        },
-    ]
+exports.sourceNodes = async ({
+  actions,
+  createNodeId,
+  createContentDigest,
+}) => {
+  const { createNode } = actions
 
-    firebase.initializeApp({ credential: firebase.credential.cert({
+  types = [
+    {
+      type: "User",
+      collection: "users",
+      map: doc => ({
+        name: doc.name,
+        email: doc.email,
+        lastOpenedOrganization___NODE: doc.lastOpenedOrganization.id,
+        notificationMode: doc.notificationMode,
+        profilePicture: doc.profilePicture,
+        phoneNumber: [doc.phoneNumber.number, doc.phoneNumber.type],
+        organizations___NODE: doc.organizations.map(org => org.id),
+      }),
+    },
+    {
+      type: "Organization",
+      collection: "organizations",
+      map: doc => ({
+        name: doc.name,
+        description: doc.description,
+        profilePicture: doc.profilePicture,
+        admin___NODE: doc.admin.id,
+        members___NODE: doc.members.map(user => user.id),
+      }),
+    },
+  ]
+
+  firebase.initializeApp({
+    credential: firebase.credential.cert({
       projectId: process.env.PROJECT_ID,
       clientEmail: process.env.CLIENT_EMAIL,
-      privateKey: process.env.PRIVATE_KEY.replace(/\\n/g, '\n')
-    })});
-    const db = firebase.firestore();
+      privateKey: process.env.PRIVATE_KEY.replace(/\\n/g, "\n"),
+    }),
+  })
+  const db = firebase.firestore()
 
-    const promises = types.map(
+  const promises = types.map(
     async ({ collection, type, populate, map = node => node }) => {
-        const snapshot = await db.collection(collection).get();
-        for (let doc of snapshot.docs) {
-        const contentDigest = getDigest(doc.id);
+      const snapshot = await db.collection(collection).get()
+      for (let doc of snapshot.docs) {
+        const contentDigest = getDigest(doc.id)
         const node = createNode(
-            Object.assign({}, map(doc.data()), {
+          Object.assign({}, map(doc.data()), {
             id: doc.id,
             parent: null,
             children: [],
             internal: {
-                type,
-                contentDigest,
+              type,
+              contentDigest,
             },
-            })
-        );
+          })
+        )
 
-        Promise.resolve();
-        }
+        Promise.resolve()
+      }
     }
-    );
+  )
 
-    await Promise.all(promises);
+  await Promise.all(promises)
 
-    return;
-  }
+  return
+}
--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -1,5 +1,5 @@
 import * as React from "react"
-import { Link, useStaticQuery, graphql } from 'gatsby'
+import { Link, useStaticQuery, graphql } from "gatsby"
 
 import Layout from "../components/layout"
 import Seo from "../components/seo"
