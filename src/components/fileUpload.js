eb80953eeb996f9969d16a5d64ea623bb7dfd49e
fjoyceline <fionajoyceline@gmail.com>
2021-09-16 21:28:42 +0700

    Make upload pretty

--- src/components/fileUpload.js
+++ src/components/fileUpload.js
@@ -4,17 +4,25 @@ import {
   Box,
   Button,
   Input,
-  InputGroup,
-  InputRightElement,
-  useToast,
+  Text,
   CircularProgress,
-  Image
+  Center,
+  Image,
+  VStack,
 } from "@chakra-ui/react"
 
-import firebase from "../../plugins/gatsby-plugin-firebase-custom";
-import { getDownloadURL, getStorage, ref, uploadBytesResumable } from "@firebase/storage";
+import firebase from "../../plugins/gatsby-plugin-firebase-custom"
+import {
+  getDownloadURL,
+  getStorage,
+  ref,
+  uploadBytesResumable,
+} from "@firebase/storage"
 
-const storage = getStorage(firebase); // Middleware for firebase storage
+import Pic from "../images/Upload/upload.png"
+import { FiUpload } from "react-icons/fi"
+
+const storage = getStorage(firebase) // Middleware for firebase storage
 
 /**
  * Gets the task when uploading files to firestore
@@ -23,117 +31,138 @@ const storage = getStorage(firebase); // Middleware for firebase storage
  * @returns {import("@firebase/storage").UploadTask}
  */
 const uploadFile = (reference, file) => {
-    if ( file != null){
-        const storageRef = ref(storage, reference+"/"+file.name);
-        return uploadBytesResumable(storageRef, file)
-    }
-    else {
-        alert ("Make sure file is uploaded")
-    }
+  if (file != null) {
+    const storageRef = ref(storage, reference + "/" + file.name)
+    return uploadBytesResumable(storageRef, file)
+  } else {
+    alert("Make sure file is uploaded")
+  }
 }
 
 /**
  * Sets the image state as the file selected
  */
 const UploadImageButton = () => {
+  const [image, setImage] = useState(null)
+  const [progress, setProgress] = useState(0)
+  const [uploadedURL, setUploadedURL] = useState("")
 
-    const [image, setImage] = useState(null)
-    const [progress, setProgress] = useState(0)
-    const [uploadedURL, setUploadedURL] = useState("")
+  const [isSelected, setSelected] = useState(false)
+  const [inProgress, setInProgress] = useState(false)
+  const [isUploaded, setUploaded] = useState(false)
 
-    const [isSelected, setSelected] = useState(false)
-    const [inProgress, setInProgress] = useState(false)
-    const [isUploaded, setUploaded] = useState(false)
+  const reference = "images" // the folder path in the firestore storage
 
-    const reference = "images" // the folder path in the firestore storage
-
-    // Handler for when the user selects a file
-    const onImageChange = (e) => {
-        const reader = new FileReader();
-        let file = e.target.files[0];
-        if (file) {
-            reader.onload = () => {
-                if (reader.readyState === 2){
-                    console.log(file)
-                    setImage(file)
-                    setSelected(true)
-                }
-            }
-            reader.readAsDataURL(e.target.files[0])
+  // Handler for when the user selects a file
+  const onImageChange = e => {
+    const reader = new FileReader()
+    let file = e.target.files[0]
+    if (file) {
+      reader.onload = () => {
+        if (reader.readyState === 2) {
+          console.log(file)
+          setImage(file)
+          setSelected(true)
         }
-        else {
-            setImage(null)
-        }
-    } 
-
-    const handleClick = () => {
-
-        const uploadTask = uploadFile(reference,image)
-        if (uploadTask == null ){
-            alert("Make sure file is uploaded")
-            return;
-        }
-        uploadTask.on("state_changed", 
-        
-            // Observer state when image is uploading
-            (snapshot) => {
-                setInProgress(true)
-                const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100
-                setProgress(progress)
-            },
-
-            // File upload has failed
-            (error) => {
-                console.log(error)
-            },
-
-            // File upload is successfull
-            () => {
-                getDownloadURL(uploadTask.snapshot.ref).then((url) => {
-                    // Attach to required document ... 
-                    setUploaded(true)
-                    setUploadedURL(url)
-                })
-            }
-        )
+      }
+      reader.readAsDataURL(e.target.files[0])
+    } else {
+      setImage(null)
     }
-  
+  }
+
+  const handleClick = () => {
+    const uploadTask = uploadFile(reference, image)
+    if (uploadTask == null) {
+      alert("Make sure file is uploaded")
+      return
+    }
+    uploadTask.on(
+      "state_changed",
+
+      // Observer state when image is uploading
+      snapshot => {
+        setInProgress(true)
+        const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100
+        setProgress(progress)
+      },
+
+      // File upload has failed
+      error => {
+        console.log(error)
+      },
+
+      // File upload is successfull
+      () => {
+        getDownloadURL(uploadTask.snapshot.ref).then(url => {
+          // Attach to required document ...
+          setUploaded(true)
+          setUploadedURL(url)
+        })
+      }
+    )
+  }
 
   return (
     <>
+      <Box
+        pos="absolute"
+        left="50%"
+        top="50%"
+        transform="translate(-50%, -50%)"
+      >
         <Box
-            style={{
-                position: 'absolute', left: '50%', top: '50%',
-                transform: 'translate(-50%, -50%)'
-            }}
+          align="center"
+          pos="absolute"
+          left="50%"
+          top="38%"
+          transform="translate(-50%, -50%)"
         >
-            <Input
-                type = "file"
-                accept = "image/x-png,image/jpeg"
-                onChange={e => onImageChange(e)}
-            />
-            <Button
-                value = "Upload file"
-                type = "Submit"
-                disabled = {!isSelected}
-                hidden = {!isSelected}
-                onClick={handleClick}
-            > Upload File </Button>
-            <CircularProgress
-                value={progress}
-                max="100"
-                hidden = {!inProgress}
-            />
-            <Image
-                hidden = {!isUploaded}
-                src = {uploadedURL}
-                alt = "Uploaded image"
-            />
+          <Image h="200px" src={Pic} />
         </Box>
-        
+        <Input
+          pt="260px"
+          pl="200px"
+          h="400px"
+          w="600px"
+          type="file"
+          accept="image/x-png,image/jpeg"
+          onChange={e => onImageChange(e)}
+        />
+        <Box
+          align="center"
+          pos="absolute"
+          left="50%"
+          top="82%"
+          transform="translate(-50%, -50%)"
+        >
+          <Text hidden={isSelected} color="gray.400">
+            Choose a file or drag it here
+          </Text>
+          <Button
+            bgColor="ripple.200"
+            color="white"
+            fontFamily="Raleway-Bold"
+            borderRadius="30px"
+            variant="solid"
+            size="md"
+            leftIcon={<FiUpload />}
+            _hover={{ transform: "scale(1.05)" }}
+            value="Upload file"
+            type="Submit"
+            disabled={!isSelected}
+            hidden={!isSelected}
+            onClick={handleClick}
+          >
+            {" "}
+            Upload File{" "}
+          </Button>
+        </Box>
+        <CircularProgress value={progress} max="100" hidden={!inProgress} />
+        <Image hidden={!isUploaded} src={uploadedURL} alt="Uploaded image" />
+      </Box>
     </>
   )
 }
 
 export default UploadImageButton
-
--- src/pages/upload.js
+++ src/pages/upload.js
@@ -1,11 +1,12 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react"
 import Layout from "../components/layout"
-import UploadImageButton from "../components/fileUpload";
+import UploadImageButton from "../components/fileUpload"
 
 const Upload = () => {
-    return( 
+  return (
     <Layout>
-        <UploadImageButton folderPath={"test"}/>
-    </Layout>)
+      <UploadImageButton folderPath={"test"} />
+    </Layout>
+  )
 }
 export default Upload
