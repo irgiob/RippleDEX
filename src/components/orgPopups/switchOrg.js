b6a107e5e3e15f92d0d232e0ab7c0ea589d2b2a3
Irgio Basrewan <irgiob@gmail.com>
2021-09-24 17:25:32 +0700

    fixed switch workspace popup to show org info and have functional searching

--- src/components/orgPopups/switchOrg.js
+++ src/components/orgPopups/switchOrg.js
@@ -1,11 +1,14 @@
 import React, { useState, useEffect } from "react"
 import { updateUser } from "../../models/User"
+import { getOrganization } from "../../models/Organisation"
+import { navigate } from "gatsby-link"
 
 import {
   Box,
-  HStack,
-  Button,
-  Center,
+  VStack,
+  Flex,
+  Avatar,
+  Badge,
   Input,
   InputGroup,
   InputLeftElement,
@@ -13,119 +16,127 @@ import {
   ModalOverlay,
   ModalContent,
   ModalCloseButton,
-  Spacer,
   Text,
-  Image,
   useToast,
 } from "@chakra-ui/react"
 
-import { RiArrowLeftRightLine } from "react-icons/ri"
-import ProfilePicture from "../../images/RippleDEXDark.svg"
-
+import ProfilePicture from "../../images/RippleDEX.png"
 import { BiSearch } from "react-icons/bi"
 
 const SwitchOrgPopup = props => {
   const toast = useToast()
-  const allOrg = props.user?.organizations
+  const [search, setSearch]   = useState("") 
+  const [orgs, setOrgs] = useState([props.org])
 
-  const orgList = allOrg.map((allOrg, i) => {
-    return (
-      <li w="100%" mt="10%" textAlign="left" key={i}>
-        <Button
-          bgColor="white"
-          h="50px"
-          textAlign="left"
-          _hover={{
-            transform: "scale(1.08)",
-          }}
-          leftIcon={<RiArrowLeftRightLine />}
-          onClick={() => {
-            updateUser(props.user?.id, {
-              lastOpenedOrganization: allOrg.toString(),
-            }).then(
-                toast({
-                  title: "Success",
-                  description: "Organization has been changed",
-                  status: "success",
-                  duration: 5000,
-                  isClosable: true,
-                }),
-                window.location.reload(false)
-              )
-          }}
-        >
-          {allOrg}
-        </Button>
-        <hr />
-      </li>
-    )
-  })
+  useEffect(() => {
+    const getOrgs = async (orgIDs) => {
+      const orgList = []
+      for await (const orgID of orgIDs) {
+        if (orgID !== props.org.id) {
+          const org = await getOrganization(orgID)
+          orgList.push(org)
+        }
+      }
+      setOrgs([props.org, ...orgList])
+    }
+    getOrgs(props.user.organizations)
+  }, [props])
 
   return (
     <Modal isOpen={props.isOpen} onClose={props.onClose}>
       <ModalOverlay />
       <ModalContent
         pos="absolute"
-        h="fit-content"
-        minHeight="50%"
+        minH="50%"
+        maxH="80%"
         maxW="30%"
-        p="15px"
-        pb="30px"
         borderRadius="15px"
         value="inside"
         scrollBehavior="inside"
       >
         <ModalCloseButton m="15px" />
-        <Center>
-          <Box textAlign="left" w="80%" mt="25px">
-            <Text
-              pb="5px"
-              fontFamily="Nunito-Bold"
-              fontSize="23px"
-              color="ripple.200"
-            >
-              Switch Organizations
-            </Text>
-            <hr w="100%" />
-            <HStack maxW="100%">
-              <Box>
-                <Spacer pb="30px" />
-                <Text pb="10px" fontSize="20px" color="ripple.200">
-                  Current Organization: <br />
-                  <Text color="black"> {props.org?.name} </Text>
-                  <Text color="grey" fontSize="15px">
-                    {" "}
-                    {props.org?.description}{" "}
-                  </Text>
-                </Text>
-              </Box>
-              <Spacer />
-              <Image
-                src={props.org?.profilePicture || ProfilePicture}
-                boxSize="100px"
-                p="20px"
-              />
-            </HStack>
-            <Text pt="10px" pb="10px" fontSize="20px" color="ripple.200">
-              All Organization <br />
-            </Text>
-            <InputGroup>
-              <InputLeftElement
-                pointerEvents="none"
-                children={<BiSearch style={{ color: "#a2b1c0" }} />}
-              />
-              <Input type="text" placeholder="Search" />
-            </InputGroup>
-            <Box pt="30px" pb="30px" w="100%">
-              <ol style={{ listStyleType: "none" }}>
-                {orgList}
-                <hr />
-              </ol>
-            </Box>
-          </Box>
-        </Center>
+        <Box textAlign="left" w="100%" h="100%" p="2em" overflow="scroll">
+          <Text
+            mb="0.5em"
+            fontFamily="Nunito-Bold"
+            fontSize="23px"
+            color="ripple.200"
+          >
+            Switch Organizations
+          </Text>
+          <hr w="100%" />
+          <InputGroup mb="1em">
+            <InputLeftElement
+              pointerEvents="none"
+              children={<BiSearch style={{ color: "#a2b1c0" }} />}
+            />
+            <Input 
+              type="text" 
+              placeholder="Search" 
+              onChange={(e) => setSearch(e.target.value.toLowerCase())}
+            />
+          </InputGroup>
+          <VStack spacing="1em" align="left">
+            {orgs
+              .filter((org) => org.name.toLowerCase().includes(search))
+              .map((org, i) => (
+                <OrgListItem
+                  key={i} 
+                  org={org}
+                  isCurrentOrg={org.id === props.org?.id}
+                  onClick={() => {
+                    updateUser(props.user?.id, {
+                      lastOpenedOrganization: org.id,
+                    }).then(
+                        toast({
+                          title: "Success",
+                          description: "Organization has been changed",
+                          status: "success",
+                          duration: 5000,
+                          isClosable: true,
+                        }),
+                        navigate("/")
+                      )
+                  }}
+                />
+              ))
+            }
+          </VStack>
+        </Box>
       </ModalContent>
     </Modal>
+  )
+}
+
+const OrgListItem = ({onClick, org, isCurrentOrg}) => {
+  var transformScale = "scale(1.05)"
+  var cursor = "pointer"
+  
+  if (isCurrentOrg) {
+    onClick = () => {}
+    transformScale = "scale(1)"
+    cursor = "default"
+  }
+
+  return (
+    <Flex 
+      _hover={{ transform: transformScale }} 
+      onClick={onClick}
+      cursor={cursor}
+    >
+      <Avatar src={org.profilePicture || ProfilePicture} />
+      <Box ml="3">
+        <Text fontWeight="bold">
+          {org.name}
+          {isCurrentOrg &&
+            <Badge ml="1" colorScheme="green">
+              Current
+            </Badge>
+          }
+        </Text>
+        <Text fontSize="sm">{org.description}</Text>
+      </Box>
+    </Flex>
   )
 }
 
