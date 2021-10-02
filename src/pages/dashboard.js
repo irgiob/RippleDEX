21eb37b2a901fb1a449ec1746909c9531c0af58b
wilbertcargeson <wilbert.cargeson@gmail.com>
2021-10-02 21:49:31 +1000

    Added doughnut chart component for deals status breakdown

--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -4,17 +4,21 @@ import Layout from "../components/layout"
 import Seo from "../components/seo"
 
 import { Heading } from "@chakra-ui/react"
+import DoughnutChart from "../analytics/doughnutChart"
 
-const DashboardPage = ({user, setUser, org, setOrg}) => {
-  return (<>
+const DashboardPage = ({ user, setUser, org, setOrg }) => {
+  return (
+    <>
       <Heading>Dashboard for {org != null ? org.name : ""}</Heading>
-  </>)
+      <DoughnutChart org={org} />
+    </>
+  )
 }
 
-const Dashboard = (props) => (
+const Dashboard = props => (
   <Layout location={props.location}>
     <Seo title="Dashboard" />
-    <DashboardPage/>
+    <DashboardPage />
   </Layout>
 )
 
