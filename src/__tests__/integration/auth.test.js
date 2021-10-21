a997da05af11438d801fa4f9e3638d7aa108c471
wilbertcargeson <wilbert.cargeson@gmail.com>
2021-10-21 17:00:07 +1100

    Added signup testing

--- src/__tests__/integration/auth.test.js
+++ src/__tests__/integration/auth.test.js
@@ -2,18 +2,21 @@ import React from "react"
 import Login from "../../components/auth/login"
 import SignUp from "../../components/auth/signup"
 import { render, fireEvent, act, waitFor } from "@testing-library/react"
-import { login, signInGoogle } from "../../utils/AuthFunctions"
+import { login, signInGoogle, signup } from "../../utils/AuthFunctions"
 import { navigate } from "gatsby-link"
 jest.mock("../../utils/AuthFunctions")
 
 describe("Integration testing for authenticaiton components", () => {
+  afterEach(() => {
+    jest.resetAllMocks()
+  })
   test("Testing login component with email password login", async () => {
-    const component = render(<Login />)
+    const { getByTestId } = render(<Login />)
 
     // Remember to add testid to relevant components
-    const emailInput = await component.getByTestId("email")
-    const passwordInput = await component.getByTestId("password")
-    const button = await component.getByTestId("button")
+    const emailInput = getByTestId("email")
+    const passwordInput = getByTestId("password")
+    const button = getByTestId("button")
 
     expect(emailInput.value).toBe("")
     expect(passwordInput.value).toBe("")
@@ -45,12 +48,53 @@ describe("Integration testing for authenticaiton components", () => {
     })
   })
 
-  // test("Testing signup component", () => {
-  //   const component = render(<SignUp />)
-  //   const emailInput = component.getByPlaceholderText("Email Address")
-  //   const firstName = component.getByPlaceholderText("First Name")
+  test("Testing signup component", async () => {
+    const { getByTestId } = render(<SignUp />)
 
-  //   // console.log(emailInput)
-  //   expect(emailInput.textContent).toBe("")
-  // })
+    // Remember to add testid to relevant components
+    const emailInput = getByTestId("email")
+    const firstNameInput = getByTestId("firstName")
+    const lastNameInput = getByTestId("lastName")
+    const passwordInput = getByTestId("password")
+    const passwordRetypeInput = getByTestId("password-retype")
+    const button = getByTestId("button")
+
+    expect(emailInput.value).toBe("")
+    expect(passwordInput.value).toBe("")
+    expect(passwordRetypeInput.value).toBe("")
+    expect(firstNameInput.value).toBe("")
+    expect(lastNameInput.value).toBe("")
+
+    fireEvent.change(emailInput, { target: { value: "test@test.com" } })
+    fireEvent.change(passwordInput, { target: { value: "Abcd1234" } })
+    fireEvent.change(passwordRetypeInput, { target: { value: "Abcd1234" } })
+    fireEvent.change(firstNameInput, { target: { value: "test" } })
+    fireEvent.change(lastNameInput, { target: { value: "test" } })
+
+    expect(emailInput.value).toBe("test@test.com")
+    expect(passwordInput.value).toBe("Abcd1234")
+    expect(passwordRetypeInput.value).toBe("Abcd1234")
+    expect(firstNameInput.value).toBe("test")
+    expect(lastNameInput.value).toBe("test")
+
+    fireEvent.click(button)
+
+    // As clicking the button changes internal state asynchronously, we would like to use waitFor
+    await waitFor(() => {
+      expect(signup).toBeCalledTimes(1)
+    })
+  })
+
+  test("Testing signup component with google login", async () => {
+    const component = render(<SignUp />)
+
+    const googleSignIn = await component.getByTestId("google")
+
+    fireEvent.click(googleSignIn)
+
+    // As clicking the button changes internal state asynchronously, we would like to use waitFor
+    await waitFor(() => {
+      expect(signInGoogle).toBeCalledTimes(1)
+    })
+  })
 })
